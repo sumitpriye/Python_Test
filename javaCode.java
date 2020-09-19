8b517f8ac554eb8b9b982ea8bde927e3c29671a5
Sumit Shrivastava <sumitsinha125@gmail.com>
2020-09-19 17:02:02 +0530

    Undoing Commit demo

--- javaCode.java
+++ javaCode.java
@@ -5,6 +5,7 @@ public static void main(string[] args)
 {
 
 System.out.println("Learning Branch Concepts in git")
+System.out.println("Learning Undoing Committed Concepts in git")
 
 int  a =5;
 int b=9;
